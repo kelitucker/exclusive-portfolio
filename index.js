00e1e0ead7f6a1792fc8161b57dad8e4b2faf0c1
kelitucker <keli.tucker@yahoo.com>
2026-08-05 12:55:01 -0500

    feat: add final modal

--- index.js
+++ index.js
@@ -23,3 +23,14 @@
         );
     })
 }
+
+let isModalOpen = false;
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove(`modal--open`);
+    }
+    isModalOpen = true;
+    document.body.classList += ` modal--open`;
+}
+
